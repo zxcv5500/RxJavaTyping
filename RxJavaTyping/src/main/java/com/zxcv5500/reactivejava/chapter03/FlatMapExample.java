52e24c115da6babc7cea0c529de804e080081008
zxcv5500 <zxcv5500@gmail.com>
2018-11-20 22:13:16 +0900

    flatMap 함수 예제 작성함

---
+++ RxJavaTyping/src/main/java/com/zxcv5500/reactivejava/chapter03/FlatMapExample.java
@@ -0,0 +1,44 @@
+package com.zxcv5500.reactivejava.chapter03;
+
+import com.zxcv5500.reactivejava.common.CommonUtils;
+import com.zxcv5500.reactivejava.common.Log;
+import com.zxcv5500.reactivejava.common.MarbleDiagram;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
+import static com.zxcv5500.reactivejava.common.Shape.RED;
+import static com.zxcv5500.reactivejava.common.Shape.GREEN;
+import static com.zxcv5500.reactivejava.common.Shape.BLUE;
+
+public class FlatMapExample implements MarbleDiagram{
+	
+	@Override
+	public void marbleDiagram() {
+		// 함수를 별도로 정의하는 것이 가장 먼저 겪어야 할 관문임
+		Function<String, Observable<String>> getDoubleDiamonds = 
+				ball -> Observable.just(ball + "<>", ball + "<>");
+				
+		String[] balls = {RED, GREEN, BLUE};
+		Observable<String> source = Observable.fromArray(balls)
+				.flatMap(getDoubleDiamonds);
+		source.subscribe(Log::i);
+		CommonUtils.exampleComplete();		
+	}
+	
+	public void flatMapLambda() {
+		//Observable안에 Observable을 넣는다는 것이 직관적이지는 않음
+		String[] balls = {RED, GREEN, BLUE};
+		Observable<String> source = Observable.fromArray(balls)
+				.flatMap(ball->Observable.just(ball+ "<>", ball + "<>"));
+		source.subscribe(Log::i);
+		CommonUtils.exampleComplete();
+	}
+	
+	public static void main(String[] args) {
+		FlatMapExample demo = new FlatMapExample();
+		demo.marbleDiagram();
+		demo.flatMapLambda();
+	}
+
+}
